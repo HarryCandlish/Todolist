bcf50c6f4ccbc9da617a0042ea9e8f8cf6543188
Harry Candlish <hcnd90@gmail.com>
2023-01-09 15:34:34 +1300

    completed basic todo list

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+// component import
+import TaskItem from './TaskItem';
 
-const TaskList = ({tasks}) => {
+// styles
+import styles from './TaskList.module.css';
+
+const TaskList = ({ tasks, deleteTask, toggleTask, enterEditMode }) => {
   return (
-    <div>
-        <h1>Task List</h1>
-    </div>
+    <ul className={styles.tasks}>
+      {tasks.sort((a, b) => b.id - a.id).map(task => (
+        <TaskItem
+          key={task.id}
+          task={task}
+          deleteTask={deleteTask}
+          toggleTask={toggleTask}
+          enterEditMode={enterEditMode}
+        />
+      ))
+      }
+    </ul>
   )
 }
-
 export default TaskList
